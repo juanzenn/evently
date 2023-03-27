8a23bad72d784fc7e7c789d46dd2bc318791ee18
Juan Alvarez <juan.alvarez@ingeniust.net>
2023-03-27 12:45:05 -0400

    update: add shadcn Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import { Button } from "~/components/ui/Button";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Button>Default</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="link">Link</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="subtle">Subtle</Button>
+    </div>
+  );
 }
