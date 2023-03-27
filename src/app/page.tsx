b1b7a4f11964f459364eae5c3da8f7deaebea0ce
Juan Alvarez <juan.alvarez@ingeniust.net>
2023-03-27 13:26:43 -0400

    update: add shadcn form components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-import { Button } from "~/components/ui/Button";
-
 export default function Home() {
-  return (
-    <div>
-      <Button>Default</Button>
-      <Button variant="destructive">Destructive</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="link">Link</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="subtle">Subtle</Button>
-    </div>
-  );
+  return <div></div>;
 }
---
+++ src/components/ui/input.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { cn } from "~/lib/utils";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        className={cn(
+          "flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-primary-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-primary-700 dark:text-slate-50 dark:focus:ring-primary-400 dark:focus:ring-offset-primary-900",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Input.displayName = "Input";
