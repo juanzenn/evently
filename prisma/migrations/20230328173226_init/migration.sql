4b5edcf72b0b468021f85b099016f8dee6859253
Juan Alvarez <juanandres140299@gmail.com>
2023-04-21 00:05:51 -0400

    fix: add PlanetScale as a DB

--- prisma/migrations/20230328173226_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Event" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "date" DATETIME NOT NULL,
-    "endDate" DATETIME,
-    "location" TEXT NOT NULL
-);
